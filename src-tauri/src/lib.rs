db04d3c1cb15e2066c35ccad82f53b1683d321dc
Travis Fantina <tfantina@gmail.com>
2025-02-21 21:44:24 -0500

    Working to get SQLX setup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,10 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri_plugin_sql::{Migration, MigrationKind};
 
+mod db_connector;
+use db_connector::{connect_to_pool, execute_query, initialize_pool, PoolWrapper};
+use std::sync::Mutex;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let migrations = vec![Migration {
@@ -20,12 +24,18 @@ pub fn run() {
 
     let db_url = "sqlite:dbexplorer.db";
     tauri::Builder::default()
+        .manage(PoolWrapper(Mutex::new(None)))
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations(db_url, migrations)
                 .build(),
         )
         .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![
+            initialize_pool,
+            connect_to_pool,
+            execute_query
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
